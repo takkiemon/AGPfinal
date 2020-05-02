923ffff6ef424cdf13a4241dbf961a5dfc99ee8d
takkiemon <takkie91@hotmail.com>
2020-05-02 16:00:01 +0200

    mesh creation: vertices are created on a grid in a coroutine and a quad is made

---
+++ Assets/CGridComponent.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class CGridComponent : MonoBehaviour
+{
+    public int xSize, ySize;
+    private Vector3[] vertices;
+    private Mesh mesh;
+
+    private void Awake()
+    {
+        StartCoroutine("Generate");
+    }
+
+    private IEnumerator Generate()
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.05f);
+
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedural Grid";
+
+        vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+        for (int i = 0, y = 0; y <= ySize; y++)
+        {
+            for (int x = 0; x <= xSize; x++, i++)
+            {
+                vertices[i] = new Vector3(x, y);
+                yield return wait;
+            }
+        }
+
+        mesh.vertices = vertices;
+
+        int[] triangles = new int[xSize * 6];
+        triangles[0] = 0;
+        triangles[1] = triangles[4] = xSize + 1;
+        triangles[2] = triangles[3] = 1;
+        triangles[5] = xSize + 2;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null)
+        {
+            return;
+        }
+        Gizmos.color = Color.yellow;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
+    }
+}
