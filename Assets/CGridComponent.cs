f6528e3e1a7f470891f5a06d177935be2876e578
takkiemon <takkie91@hotmail.com>
2020-05-02 16:31:37 +0200

    creating all quads on resizable grid works. To Do: remove coroutine

--- Assets/CGridComponent.cs
+++ Assets/CGridComponent.cs
@@ -27,18 +27,24 @@ public class CGridComponent : MonoBehaviour
             for (int x = 0; x <= xSize; x++, i++)
             {
                 vertices[i] = new Vector3(x, y);
-                yield return wait;
             }
         }
 
         mesh.vertices = vertices;
 
-        int[] triangles = new int[xSize * 6];
-        triangles[0] = 0;
-        triangles[1] = triangles[4] = xSize + 1;
-        triangles[2] = triangles[3] = 1;
-        triangles[5] = xSize + 2;
-        mesh.triangles = triangles;
+        int[] triangles = new int[xSize * ySize * 6];
+        for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
+        {
+            for (int x = 0; x < xSize; x++, ti += 6, vi++)
+            {
+                triangles[ti] = vi;
+                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+                triangles[ti + 5] = vi + xSize + 2;
+                mesh.triangles = triangles;
+                yield return wait;
+            }
+        }
         mesh.RecalculateNormals();
     }
 
